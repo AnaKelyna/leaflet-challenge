a21822b3d60c428b66cca7c77d0844cf4a47fa5f
AnaKelyna <64177211+AnaKelyna@users.noreply.github.com>
2020-09-13 22:10:59 -0500

    Update logic.js

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -2,13 +2,14 @@
 // Store our API endpoint inside queryUrl
 var queryUrl = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2020-08-15&endtime=" +
     "2020-08-22&maxlongitude=-69.52148437&minlongitude=-123.83789062&maxlatitude=48.74894534&minlatitude=25.16517337";
+
 function getColor(d) {
-        return d > 5 ? '#F06B6B' :
-               d > 4  ? '#F0A76B' :
-               d > 3  ? '#F3BA4D' :
-               d > 2  ? '#F4DB4E' :
-               d > 1   ? '#E1F34D' :
-                          '#B7F44E';
+    return d > 5 ? '#F06B6B' :
+        d > 4 ? '#F0A76B' :
+        d > 3 ? '#F3BA4D' :
+        d > 2 ? '#F4DB4E' :
+        d > 1 ? '#E1F34D' :
+        '#B7F44E';
 };
 // Perform a GET request to the query URL
 d3.json(queryUrl, function(data) {
@@ -27,38 +28,39 @@ function createFeatures(earthquakeData) {
     // Run the onEachFeature function once for each piece of data in the array
     var earthquakes = L.geoJSON(earthquakeData, {
         onEachFeature: onEachFeature,
-        pointToLayer: function(features,latlng){
-            return L.circle(latlng,{
-              radius: features.properties.mag *20000 + 10000 ,
-              color: 'black',
-              fillColor: getColor(features.properties.mag),
-              fillOpacity: 0.8,
-              opacity: 0.7,
-              weight: 1
+        pointToLayer: function(features, latlng) {
+            return L.circle(latlng, {
+                radius: features.properties.mag * 20000 + 10000,
+                color: 'black',
+                fillColor: getColor(features.properties.mag),
+                fillOpacity: 0.8,
+                opacity: 0.7,
+                weight: 1
 
-        }); 
-    }
-        
-        
+            });
+        }
+
+
     });
     // Sending our earthquakes layer to the createMap function
     createMap(earthquakes);
     //  earthquakes.addLayer(circle); //
 }
 
-var legend = L.control({position: "bottomright"});
-    legend.onAdd = function(){
-      var div = L.DomUtil.create("div","info legend");
-      var limits = ["0-1","1-2","2-3","3-4","4-5","+5"];
-      var colors = ['#B7F44E','#E1F34D','#F4DB4E','#F3BA4D','#F0A76B','#F06B6B'];
+var legend = L.control({ position: "bottomright" });
+legend.onAdd = function() {
+    var div = L.DomUtil.create("div", "info legend");
+    var limits = ["0-1", "1-2", "2-3", "3-4", "4-5", "+5"];
+    var colors = ['#B7F44E', '#E1F34D', '#F4DB4E', '#F3BA4D', '#F0A76B', '#F06B6B'];
 
     for (var i = 0; i < limits.length; i++) {
-      div.innerHTML +=
-          '<i style="background:' + colors[i]+ '"></i> ' + limits[i]+ '<br>'};
+        div.innerHTML +=
+            '<i style="background:' + colors[i] + '"></i> ' + limits[i] + '<br>'
+    };
 
     return div;
-  };
-  
+};
+
 
 function createMap(earthquakes) {
     // Define streetmap and darkmap layers
@@ -67,7 +69,7 @@ function createMap(earthquakes) {
         tileSize: 512,
         maxZoom: 18,
         zoomOffset: -1,
-        id: "mapbox/streets-v11",
+        id: "mapbox/light-v9",
         accessToken: API_KEY
     });
     // Define a baseMaps object to hold our base layers
@@ -83,8 +85,5 @@ function createMap(earthquakes) {
         zoom: 3,
         layers: [streetmap, earthquakes]
     });
-    // Create a layer control
-    // Pass in our baseMaps and overlayMaps
-    // Add the layer control to the map
     legend.addTo(myMap);
 }
